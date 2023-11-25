3dd14480bb561b8fae0a971fb7226f081562ad1c
Yusuf <af003yusuf@gmail.com>
2023-11-26 06:53:51 +0700

    .

---
+++ home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd 
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# aircraft calculator! 👋")
+
+st.sidebar.success("Select a menu above.")
+
+st.markdown(
+    """
+    still under construction, updates are underway
+    
+    """ )
